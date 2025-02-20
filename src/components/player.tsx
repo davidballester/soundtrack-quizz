a9320f1d1d8e4c773f1460692364615b2838bfae
David Ballester <david.ballester.mena@gmail.com>
2025-02-20 18:07:32 +0100

    Fix all players running at once

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -53,7 +53,7 @@ export function Player({
   durationInSeconds?: number;
 }) {
   const [playing, setPlaying] = useState<boolean>(false);
-  const { player, started } = usePlayer({ videoId });
+  const { player, ready, started } = usePlayer({ videoId });
   const play = useCallback(() => {
     if (!player) {
       return;
@@ -76,11 +76,12 @@ export function Player({
     <div className={`player ${started ? "started" : ""}`}>
       <div className={`blur-overlay ${resolved ? "hidden" : ""}`}>
         <Button
-          size="xl"
+          size="2xl"
           onClick={play}
-          disabled={resolved || playing}
-          loading={playing}
+          disabled={resolved || playing || !ready}
+          loading={playing || !ready}
           title="Play"
+          style={{ cursor: "pointer" }}
         >
           <LuPlay />
         </Button>
@@ -89,7 +90,7 @@ export function Player({
         id={videoId}
         width={width}
         height={height}
-        src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${location.origin}&autoplay=1&controls=0&fs=0&playsinline=1&disablekb=1&color=white`}
+        src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=${location.origin}&autoplay=0&controls=0&fs=0&playsinline=1&disablekb=1&color=white`}
       ></iframe>
     </div>
   );
@@ -97,17 +98,20 @@ export function Player({
 
 function usePlayer({ videoId }: { videoId: string }): {
   player: YT.Player | null;
+  ready: boolean;
   started: boolean;
 } {
   const youTubeApiReady = useContext(YouTubeApiReadyContext);
   const [player, setPlayer] = useState<YT.Player | null>(null);
   const [started, setStarted] = useState<boolean>(false);
+  const [ready, setReady] = useState<boolean>(false);
   useEffect(() => {
     if (!youTubeApiReady) {
       return;
     }
     const newPlayer = new YT.Player(videoId, {
       events: {
+        onReady: () => setReady(true),
         onStateChange: () => {
           if (
             started ||
@@ -121,6 +125,6 @@ function usePlayer({ videoId }: { videoId: string }): {
     });
     setPlayer(newPlayer);
     return () => newPlayer.stopVideo();
-  }, [youTubeApiReady, videoId]);
-  return { player, started };
+  }, [youTubeApiReady, videoId, setStarted, setReady]);
+  return { player, ready, started };
 }
